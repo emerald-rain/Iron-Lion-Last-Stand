fe9cb1dd2693323861019511e7a8d3c14aec8e4c
malysh-ka <52384128+emerald-rain@users.noreply.github.com>
2023-10-13 16:00:37 +0300

    NetworkManagerUI Buttons fix

--- Assets/02_Scripts/03 Other/NetworkManagerUI.cs
+++ Assets/02_Scripts/03 Other/NetworkManagerUI.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using Unity.Netcode;
+
+using UnityEngine.UI; // for the buttons
+using Unity.Netcode; // to control the NetworkManager
 
 public class NetworkManagerUI : MonoBehaviour
 {
@@ -10,9 +11,10 @@ public class NetworkManagerUI : MonoBehaviour
     [SerializeField] private Button hostButton;
     [SerializeField] private Button clientButton;
 
-    private void Avake() {
+    private void Awake() {
         // Start a server
         serverButton.onClick.AddListener(() => {
+            Debug.Log("Button clicked");
             NetworkManager.Singleton.StartServer();
         });
         // Start a host 

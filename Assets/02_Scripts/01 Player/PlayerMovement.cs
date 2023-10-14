a449650915ab549dcd943aa96310580e62823f53
malysh-ka <52384128+emerald-rain@users.noreply.github.com>
2023-10-14 18:59:22 +0300

    Server validated PlayerMovement
    The main changes came to PlayerMovement.cs, the client movement is highly fluid for some reason.

--- Assets/02_Scripts/01 Player/PlayerMovement.cs
+++ Assets/02_Scripts/01 Player/PlayerMovement.cs
@@ -1,38 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-using Unity.Netcode; // for owned obj movement
+using Unity.Netcode; 
 
 public class PlayerMovement : NetworkBehaviour
 {
-    // https://www.youtube.com/watch?v=3Uc3cscnYns&t=53s&ab_channel=MoreBBlakeyyy
-    // movement tutorial
-
     private Rigidbody2D rb;
-    float vertical;
-    float horizontal;
+    private float vertical;
+    private float horizontal;
 
-    public float moveSpeed;
-    public float speedLimit = 0.7f;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float speedLimit = 0.7f;
 
-    void Start() {
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update() {
-        if (!IsOwner) return;
+    void Update()
+    {
+        // Only process movement for the owner of this object
+        if (!IsOwner)
+            return;
 
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
+        // Handle the movement for the server authoritative player
+        HandleMovementServerAuth();
     }
 
-    void FixedUpdate() {
-        if(horizontal != 0 && vertical != 0) {
+    private void HandleMovementServerAuth()
+    {
+        // Get the horizontal and vertical input from the player
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        // Log the input (for debugging purposes)
+        // Debug.Log(horizontal + " " + vertical);
+
+        // Call the server RPC to handle movement
+        HandleMovementServerRpc(horizontal, vertical);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void HandleMovementServerRpc(float horizontal, float vertical)
+    {
+        // Apply speed limit if moving diagonally
+        if (horizontal != 0 && vertical != 0)
+        {
             horizontal *= speedLimit;
             vertical *= speedLimit;
         }
 
+        // Set the velocity of the rigidbody for movement
         rb.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
-    }
+    } 
 }
--- Assets/02_Scripts/01 Player/PlayerShooting.cs
+++ Assets/02_Scripts/01 Player/PlayerShooting.cs
@@ -5,49 +5,66 @@ using Unity.Netcode;
 
 public class PlayerShooting : MonoBehaviour
 {
-    // https://youtu.be/-bkmPm_Besk?si=mg8sesMm4krQZtBw
-    // thank you for shooting system tutorial
-
-    // Gun rotation
     private Camera mainCam;
     private Vector3 mousePos;
 
-    // Shooting the bullets
-    public GameObject bullet;
-    public Transform bulletTransform;
-    public bool canFire;
-    private float timer;
-    public float timeBetweenFiring;  
+    public GameObject bullet;  // Prefab for the bullet
+    public Transform bulletTransform;  // Transform to spawn the bullet
 
-    void Start() {
+    public bool canFire;  // Flag indicating if the player can fire
+    private float timer;  // Timer to control firing rate
+    public float timeBetweenFiring;  // Time between each firing
+
+    void Start()
+    {
+        // Get the main camera
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
-    void Update() {
+    void Update()
+    {
+        // Get the mouse position in the game world
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
         Vector3 rotation = mousePos - transform.position;
 
+        // Calculate rotation angle for aiming
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, rotZ);
 
-        if (!canFire) {
+        if (!canFire)
+        {
+            // Increment the timer until the player can fire again
             timer += Time.deltaTime;
-            if(timer > timeBetweenFiring)
+            if (timer > timeBetweenFiring)
             {
+                // Player can fire again
                 canFire = true;
                 timer = 0;
             }
         }
 
-        if (Input.GetMouseButton(0) && canFire) {
-            canFire = false;
+        if (Input.GetMouseButton(0) && canFire)
+        {
+            canFire = false;  // Player fired
 
-            // Instantiate a bullet at the bulletTransform position with no rotation.
             GameObject spawnedBulletObject = Instantiate(bullet, bulletTransform.position, Quaternion.identity);
-            // Get the Transform component of the spawned bullet object.
             Transform spawnedBulletTransform = spawnedBulletObject.transform;
-            // Call the Spawn method of NetworkObject on the spawned bullet with true as a parameter.
+
+            // Spawn the bullet over the network
             spawnedBulletTransform.GetComponent<NetworkObject>().Spawn(true);
+            SendMessageToServer("BulletSpawned");
         }
     }
+
+    [ServerRpc]
+    private void SendMessageToServer(string message) 
+    {
+        Debug.Log("SendMessageToServer");
+    }
+
+    [ClientRpc]
+    private void SendMessageToClient(string message) {
+    // Handle the message (e.g., display it on the client)
+    Debug.Log("Received confirmation on the client: " + message);
+}
 }

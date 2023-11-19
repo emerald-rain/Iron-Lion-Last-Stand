f6beebfe5256ddc9877e6d2cf52e6a83fbce3ab9
malysh-ka <52384128+emerald-rain@users.noreply.github.com>
2023-11-19 21:41:26 +0200

    Redrawn sprite, added animations

--- Assets/1 Prefabs & Scripts/[Enemy] Chaser/ChaserEnemy.cs
+++ Assets/1 Prefabs & Scripts/[Enemy] Chaser/ChaserEnemy.cs
@@ -3,27 +3,40 @@ using UnityEngine.AI;
 
 public class ChaserEnemy : MonoBehaviour
 {
-    private Transform target;
+    [SerializeField] private Transform target;
+
     private NavMeshAgent agent;
-
-    void Awake()
-    {
-        agent = GetComponent<NavMeshAgent>();
-        agent.updateRotation = false;
-    }
+    private Animator animator;
     
-    void Start()
-    {
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
         GameObject player = GameObject.FindGameObjectWithTag("Player");
-        target = player.transform;
+        if (player != null)
+            target = player.transform;
     }
 
-    void Update()
-    {
+    void Update() {
         if (target != null && agent.isActiveAndEnabled)
-        {
             agent.SetDestination(target.position);
-        }
+            UpdateAnimationAndFlip();
+        
         transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    private void UpdateAnimationAndFlip() {
+        bool isWalking = agent.remainingDistance > agent.stoppingDistance;
+        animator.SetBool("isWalking", isWalking);
+
+        if (isWalking)
+        {
+            Vector3 direction = (agent.destination - transform.position).normalized;
+
+            if (direction.x < 0) // LEFT
+                transform.localScale = new Vector3(-1, 1, 1);
+            else if (direction.x > 0) // RIGHT
+                transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 }

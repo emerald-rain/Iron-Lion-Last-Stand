d9282aacb0f3c061b5f6cc788b4285855c9c5c39
malysh-ka <52384128+emerald-rain@users.noreply.github.com>
2023-12-27 11:45:39 +0200

    Reworked sound playback script and latest build

--- Assets/1 Prefabs & Scripts/[System]/GameManager.cs
+++ Assets/1 Prefabs & Scripts/[System]/GameManager.cs
@@ -1,22 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // References to UI components
+    [Header("GameOver Screen")]
     public GameOverScreen GameOverScreen;
     public ScoreManager ScoreManager;
+
+    // Manages interactions with Playfab for score handling
+    [Header("Playfab Score Sending")]
     public PlayfabManager PlayfabManager;
+
+    // Controls the playback of the game over video
+    [Header("Play GameOver Video")]
     public VidPlayer vidPlayer;
+
+    // Hides bullets during the GameOver screen
+    [Header("Hide Bullets on GameOver Screen")]
     public Transform reloadSystem;
 
-    public void GameOver() { 
+    // Manages background soundtracks
+    [Header("Background Soundtracks")]
+    public SoundEffectsPlayer soundEffectsPlayer;
+
+    void Start()
+    {
+        // Start playing background music
+        soundEffectsPlayer.PlayInRow();
+    }
+
+    public void GameOver()
+    { 
+        // Hide ammo and play a loss video
         reloadSystem.gameObject.SetActive(false);
         vidPlayer.PlayVideo();
 
+        // Get the final score and update the GameOver screen
         int score = ScoreManager.GetScore();
         GameOverScreen.Setup(score);
+
+        // Send the player's score to Playfab leaderboard
         PlayfabManager.SendLeaderboard(score); 
     }
-
 }
--- Assets/1 Prefabs & Scripts/[System]/SFXSystem/SoundEffectsPlayer.cs
+++ Assets/1 Prefabs & Scripts/[System]/SFXSystem/SoundEffectsPlayer.cs
@@ -1,34 +1,46 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundEffectsPlayer : MonoBehaviour
 {
+    // Adjustable parameters
     public float volume = 1.0f;
     public int maxAudioSources = 1;
-    private List<AudioSource> audioSourcesList = new List<AudioSource>();
     public AudioClip[] audioClips;
+
+    // Internal variables
+    private AudioSource[] audioSources;
 
     void Start()
     {
+        // Initialize audio sources on startup
         InitializeAudioSources();
     }
 
+    // Initialize audio sources and set their volumes
     private void InitializeAudioSources()
     {
+        audioSources = new AudioSource[maxAudioSources];
+
         for (int i = 0; i < maxAudioSources; i++)
         {
-            AudioSource newSource = gameObject.AddComponent<AudioSource>();
-            newSource.volume = volume;
-            audioSourcesList.Add(newSource);
+            // Add AudioSource component to the game object
+            audioSources[i] = gameObject.AddComponent<AudioSource>();
+            // Set volume for each AudioSource
+            audioSources[i].volume = volume;
         }
     }
 
+    // Play a random audio clip immediately
     public void PlayRandom()
     {
+        // Get the next random audio clip
         AudioClip clipToPlay = GetNextClip();
-
+        
+        // Get an available AudioSource
         AudioSource availableSource = GetAvailableAudioSource();
+
+        // Play the audio clip on the available AudioSource
         if (availableSource != null)
         {
             availableSource.clip = clipToPlay;
@@ -36,22 +48,54 @@ public class SoundEffectsPlayer : MonoBehaviour
         }
     }
 
+    // Begin playing background tracks
+    public void PlayInRow()
+    {
+        // Start a coroutine for continuous background track playback
+        StartCoroutine(PlayInRowCoroutine());
+    }
+
+    // Coroutine for playing tracks in a continuous loop
+    private IEnumerator PlayInRowCoroutine()
+    {
+        while (true)
+        {
+            // Get the next random audio clip
+            AudioClip nextClip = GetNextClip();
+            // Get an available AudioSource
+            AudioSource availableSource = GetAvailableAudioSource();
+
+            // Play the audio clip on the available AudioSource
+            if (availableSource != null)
+            {
+                availableSource.clip = nextClip;
+                availableSource.Play();
+            }
+
+            // Wait for the duration of the audio clip
+            yield return new WaitForSeconds(nextClip.length);
+        }
+    }
+
+    // Get an available AudioSource from the array or create a new one
     private AudioSource GetAvailableAudioSource()
     {
-        foreach (AudioSource source in audioSourcesList)
+        foreach (AudioSource source in audioSources)
         {
+            // Check if the AudioSource is not currently playing
             if (!source.isPlaying)
             {
                 return source;
             }
         }
-        // Если все источники заняты, создаем новый
+
+        // Create a new AudioSource if all are occupied
         AudioSource newSource = gameObject.AddComponent<AudioSource>();
         newSource.volume = volume;
-        audioSourcesList.Add(newSource);
         return newSource;
     }
 
+    // Get a random audio clip from the array
     private AudioClip GetNextClip()
     {
         int randomIndex = Random.Range(0, audioClips.Length);
